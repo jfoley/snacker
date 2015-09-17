1e7502b87a8c59c5c5bf3f84bd7ff77aaead15cd
John Foley <john@hisfoleyness.com>
2015-09-16 19:56:04 -0700

    fix importing of dependencies for web app

--- web/server.js
+++ web/server.js
@@ -1,9 +1,3 @@
 import Server from './index';
-import MeetingRepo from '../persistance/meeting-repo';
 
-let dependencies = {
-  meetingRepo: new MeetingRepo()
-}
-dependencies.meetingRepo.meetings.push({name: 'yo'})
-
-Server.run(dependencies);
+Server.run();
